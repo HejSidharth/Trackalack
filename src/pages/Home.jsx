6ea3e4c8a03eacc19570026946a54ffc1a243a65
HejSidharth <143037077+HejSidharth@users.noreply.github.com>
2024-01-11 00:13:57 -0600

    screen bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 text-center">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 text-center min-h-screen">
         <section className="pt-24">
           <div className="px-12 mx-auto max-w-7xl">
             <motion.h1
